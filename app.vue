abe722e64446ee3e549730d90e32f9d9b74e0a1c
Sonu Singh <bhpcv252@gmail.com>
2024-02-27 09:05:55 +0530

    Project Setup and Structured

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+    <NuxtLayout>
+        <NuxtPage/>
+    </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+    title: "Sonu's Portfolio",
+    ogTitle: "Sonu's Portfolio",
+    description: "I'm all about making the web look awesome and function smoothly! From crafting beautiful HTML5 and CSS designs to bringing them to life with JavaScript and TypeScript magic, I've got you covered. Whether it's adding that extra flair with SCSS and SASS or diving deep into frameworks like React and Vue, I'm here to make your projects shine. I've even dabbled in cool animation stuff with GSAP and Three.js!",
+    ogDescription: "I'm all about making the web look awesome and function smoothly! From crafting beautiful HTML5 and CSS designs to bringing them to life with JavaScript and TypeScript magic, I've got you covered. Whether it's adding that extra flair with SCSS and SASS or diving deep into frameworks like React and Vue, I'm here to make your projects shine. I've even dabbled in cool animation stuff with GSAP and Three.js!",
+})
+</script>
