edddf20ea5b72715aaff8313758f13094ca8d4d4
Sonu Singh <bhpcv252@gmail.com>
2024-03-03 00:42:17 +0530

    Error page done

---
+++ components/Grid/GridCol.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+const props = defineProps({
+    from: String,
+    to: String
+})
+</script>
+
+<template>
+    <div class="grid-col">
+        <slot></slot>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.grid-col {
+    grid-column: v-bind(from)/v-bind(to);
+}
+</style>
--- error.vue
+++ error.vue
@@ -1,14 +1,105 @@
 <script setup lang="ts">
+
 import type { NuxtError } from '#app'
 
 const props = defineProps({
     error: Object as () => NuxtError
 })
+
+const isHovering: Ref<boolean> = ref(false)
+
 </script>
 
 <template>
-    <div>
-    <h1>{{ error?.statusCode }}</h1>
-    <NuxtLink to="/">Go back home</NuxtLink>
+    <div class="container">
+        <h2>{{ error?.statusCode }}</h2>
+        <p class="moneta-195-bold message">{{ error?.statusCode === 404? 'Page Not Found' : error?.statusMessage }}</p>
+        <GridRow>
+            <GridCol from="6" to="8">
+                <div class="centered-wrapper">
+                    <p class="para-top">
+                        Nothing to see here except a cat chilling
+                    </p>
+                    <!-- Using opacity to hide to aviod flickering -->
+                    <div class="img-wrapper">
+                        <img src="~/assets/images/cat-screaming.png" alt="Cat screaming to stop you"/>
+                        <img src="~/assets/images/cat-normal.png" alt="Cat looking at you" :class="{hide: isHovering}"/>
+                    </div>
+                    <p class="para-bottom">
+                        Keep looking or <NuxtLink to="/" @mouseover="isHovering = true" @mouseleave="isHovering = false">go home</NuxtLink>
+                    </p>
+                </div>
+            </GridCol>
+        </GridRow>
+        <PlusIcon class="top-right"/>
+        <PlusIcon class="bottom-left"/>
     </div>
 </template>
+
+<style lang="scss" scoped>
+.container {
+    position: relative;
+    height: 100vh;
+    display: grid;
+    align-items: center;
+}
+
+h2, .message {
+    position: absolute;
+}
+
+h2 {
+    top: utils.pxToRem(30);
+    left: utils.pxToRem(30);
+}
+
+.message {
+    bottom: utils.pxToRem(30);
+    right: utils.pxToRem(30);
+    line-height: 1;
+}
+
+.centered-wrapper {
+    text-align: center;
+    display: grid;
+    justify-items: center;
+    gap: utils.pxToRem(45);
+    transform: translate(0, utils.pxToRem(-80));
+}
+
+.img-wrapper {
+    width: utils.pxToRem(254);
+    height: utils.pxToRem(287);
+    position: relative;
+
+    img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        object-fit: cover;
+
+        &.hide {
+            opacity: 0;
+        }
+    }
+}
+
+a {
+    display: inline-block;
+    position: relative;
+    padding-bottom: utils.pxToRem(3);
+
+    &::after {
+        content: '';
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        height: 1px;
+        width: 100%;
+        background-color: variables.$textColor;
+    }
+}
+
+</style>
