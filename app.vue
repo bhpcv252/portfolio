c3286dbabd5ce9acf0f21b9aafdf5389049e540d
Sonu Singh <bhpcv252@gmail.com>
2024-04-16 09:32:54 +0530

    updated description

--- app.vue
+++ app.vue
@@ -2,8 +2,8 @@
 
 useHead({
     meta: [
-        { name: 'description', content: "I'm all about making the web look awesome and function smoothly! From crafting beautiful HTML5 and CSS designs to bringing them to life with JavaScript and TypeScript magic, I've got you covered. Whether it's adding that extra flair with SCSS and SASS or diving deep into frameworks like React and Vue, I'm here to make your projects shine. I've even dabbled in cool animation stuff with GSAP and Three.js!" },
-        { name: 'ogDescription', content: "I'm all about making the web look awesome and function smoothly! From crafting beautiful HTML5 and CSS designs to bringing them to life with JavaScript and TypeScript magic, I've got you covered. Whether it's adding that extra flair with SCSS and SASS or diving deep into frameworks like React and Vue, I'm here to make your projects shine. I've even dabbled in cool animation stuff with GSAP and Three.js!" }
+        { name: 'description', content: "I'm a seasoned web developer with a knack for crafting stunning frontend and rock-solid backend solutions. Over the years, I've had the privilege of collaborating with notable clients like Equinox and Tlnthub, bringing their digital visions to life with my expertise." },
+        { name: 'ogDescription', content: "I'm a seasoned web developer with a knack for crafting stunning frontend and rock-solid backend solutions. Over the years, I've had the privilege of collaborating with notable clients like Equinox and Tlnthub, bringing their digital visions to life with my expertise." }
     ],
     titleTemplate: (titleChunk) => {
         return titleChunk ? `${titleChunk} - Sonu's Portfolio` : "Sonu's Portfolio";
